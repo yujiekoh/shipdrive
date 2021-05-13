9da4df4f00082f6900f940dc2ce7b8d3b71fabbe
yujiekoh <kohyujie95@gmail.com>
2021-05-13 14:00:40 +0800

    edited login button

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -4,9 +4,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
+
+    let redirectUri = "";
+
+    if (process.env.NODE_ENV === "production") {
+      redirectUri = "https://shipdrive.herokuapp.com/dashboard";
+    } else {
+      redirectUri = "http://localhost:3000/dashboard";
+    }
+
     return (
       <Button type="primary" onClick={() => loginWithRedirect({
-          redirectUri: "http://localhost:3000/dashboard"
+          redirectUri: redirectUri
       })}>
         Login
       </Button>
